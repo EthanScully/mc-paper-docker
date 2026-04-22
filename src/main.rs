a9ff1116ba127e219f8347399df7d186c701752a
EthanScully <89865501+EthanScully@users.noreply.github.com>
2026-04-22 05:09:39 -0400

    added minecraft control

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,51 @@
 mod api;
 mod config;
 mod err;
+mod processes;
 mod utils;
 
-use std::{
-    io::Read,
-    path::Path,
-    sync::{Arc, OnceLock, RwLock},
-    *,
-};
+use crate::err::ErrorCaller;
+use std::{path::Path, *};
 
-use crate::err::{Caller, ErrorCaller};
-
-type Hresult<T> = result::Result<T, String>;
-
-fn mc() -> utils::Result<bool> {
+fn mc(updated: &mut bool) -> utils::Result<()> {
     let mc_json_path = Path::new("mc-config.json");
     let mc_jar_path = Path::new("mc.jar");
     let mc_config = config::load_json(mc_json_path).e()?;
     let latest_version = match mc_config.check_update().e()? {
         Some(o) => o,
-        None => return Ok(false),
+        None => return Ok(()),
     };
     let jar_download = latest_version.download().e()?;
     fs::write(mc_jar_path, jar_download).e()?;
+    *updated = true;
     latest_version.write_json(mc_json_path).e()?;
-    Ok(true)
+    Ok(())
 }
 fn main() {
     let schedule = utils::parse_cron(env::args()).unwrap();
     let mut upcoming = schedule.upcoming(chrono::Local);
-    sys_update().unwrap();
-    mc().unwrap();
-    mc_restart();
+    processes::sys_update().unwrap();
+    mc(&mut false).unwrap();
+    let mut mc_state = processes::new();
+    processes::mc_restart(&mut mc_state, &[""]).unwrap();
     loop {
         if let Some(next) = upcoming.next() {
             let sleep_duration = (next - chrono::Local::now()).to_std().unwrap_or_default();
             thread::sleep(sleep_duration);
-            sys_update().unwrap();
-            let updated = match mc() {
+            match processes::sys_update() {
                 Ok(r) => r,
-                Err(e) => {
-                    eprintln!("{:#?}", e);
-                    false
-                }
+                Err(e) => eprintln!("{:#?}", e),
+            };
+            let mut updated = false;
+            match mc(&mut updated) {
+                Ok(r) => r,
+                Err(e) => eprintln!("{:#?}", e),
             };
             if updated {
-                mc_restart();
+                processes::mc_restart(&mut mc_state, &[""]).unwrap();
             }
+        } else {
+            panic!("None")
         }
     }
 }
-fn sys_update() -> utils::Result<()> {
-    run_simple_command("apt", &["upgrade"]).e()?;
-    run_simple_command("apt", &["install", "openjdk-21-jre-headless", "f"]).e()?;
-    Ok(())
-}
-fn run_simple_command(arg0: &str, args: &[&str]) -> utils::Result<()> {
-    let mut command = process::Command::new(arg0);
-    for arg in args {
-        _ = command.arg(arg)
-    }
-    let mut child = command
-        .stderr(process::Stdio::piped())
-        .stdout(process::Stdio::piped())
-        .spawn()
-        .e()?;
-    let mut stdout = child.stdout.take().o()?;
-    let mut stderr = child.stderr.take().o()?;
-    let stdout_handle = thread::spawn(move || -> Hresult<String> {
-        let mut out = String::new();
-        match stdout.read_to_string(&mut out).e() {
-            Err(e) => return Err(e.to_string()),
-            _ => (),
-        };
-        Ok(out)
-    });
-    let stderr_handle = thread::spawn(move || -> Hresult<String> {
-        let mut out = String::new();
-        match stderr.read_to_string(&mut out).e() {
-            Err(e) => return Err(e.to_string()),
-            _ => (),
-        };
-        Ok(out)
-    });
-    let stdout = stdout_handle.join().o()??;
-    let stderr = stderr_handle.join().o()??;
-    if let Some(exit_status) = child.try_wait().e()? {
-        if !exit_status.success() {
-            return Err(err::new(format!("{}{}", stdout.trim(), stderr.trim())))?;
-        }
-    }
-    return Ok(());
-}
-
-
-static MC_STATE: OnceLock<Arc<RwLock<String>>> = OnceLock::new();
-
-fn mc_restart() {}
---
+++ src/processes.rs
@@ -0,0 +1,97 @@
+use crate::{
+    err::{self, Caller, ErrorCaller},
+    utils,
+};
+use std::{
+    io::{Read, Write},
+    *,
+};
+pub fn sys_update() -> utils::Result<()> {
+    run_simple_command("apt", &["upgrade"]).e()?;
+    run_simple_command("apt", &["install", "openjdk-21-jre-headless", "f"]).e()?;
+    Ok(())
+}
+fn run_simple_command(arg0: &str, args: &[&str]) -> utils::Result<()> {
+    let mut command = process::Command::new(arg0);
+    for arg in args {
+        _ = command.arg(arg)
+    }
+    let mut child = command
+        .stderr(process::Stdio::piped())
+        .stdout(process::Stdio::piped())
+        .spawn()
+        .e()?;
+    let mut stdout = child.stdout.take().o()?;
+    let mut stderr = child.stderr.take().o()?;
+    let stdout_handle = thread::spawn(move || -> Result<String, String> {
+        let mut out = String::new();
+        match stdout.read_to_string(&mut out).e() {
+            Err(e) => return Err(e.to_string()),
+            _ => (),
+        };
+        Ok(out)
+    });
+    let stderr_handle = thread::spawn(move || -> Result<String, String> {
+        let mut out = String::new();
+        match stderr.read_to_string(&mut out).e() {
+            Err(e) => return Err(e.to_string()),
+            _ => (),
+        };
+        Ok(out)
+    });
+    let stdout = stdout_handle.join().o()??;
+    let stderr = stderr_handle.join().o()??;
+    if let Some(exit_status) = child.try_wait().e()? {
+        if !exit_status.success() {
+            return Err(err::new(format!("{}{}", stdout.trim(), stderr.trim())))?;
+        }
+    }
+    return Ok(());
+}
+
+pub struct Process {
+    child: process::Child,
+    stdin: process::ChildStdin,
+}
+
+pub fn new() -> Option<Process> {
+    None
+}
+pub fn mc_restart(mc_state: &mut Option<Process>, args: &[&str]) -> utils::Result<()> {
+    if let Some(mut state) = mc_state.take() {
+        if state.child.try_wait().e()?.is_none() {
+            let msg = "/stop\n";
+            state.stdin.write_all(msg.as_bytes()).e()?;
+            state.stdin.flush().e()?;
+            let mut exited = false;
+            for _ in 0..300 {
+                if state.child.try_wait().e()?.is_some() {
+                    exited = true;
+                    break;
+                }
+                thread::sleep(time::Duration::from_secs(1));
+            }
+            if !exited {
+                state.child.kill().e()?;
+            }
+            state.child.wait().e()?;
+        }
+    }
+    let mut command = process::Command::new("java");
+    for arg in args {
+        _ = command.arg(arg)
+    }
+    let mut child = command
+        .stdin(process::Stdio::piped())
+        .stdout(process::Stdio::inherit())
+        .stderr(process::Stdio::inherit())
+        .spawn()
+        .e()?;
+    let child_stdin = child.stdin.take().o()?;
+    let state = Process {
+        child,
+        stdin: child_stdin,
+    };
+    *mc_state = Some(state);
+    Ok(())
+}
